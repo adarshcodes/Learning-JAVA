e20985463a4f7031d2e945308e9257dccf6ab68b
Adarsh Pratap <adarsh.lrnr@gmail.com>
2020-04-23 22:06:48 +0530

    Object and Classes

--- src/oops/MainClass.java
+++ src/oops/MainClass.java
@@ -1,21 +1,71 @@
 package oops;
 
-class cat {
-	boolean hasfur;
-	String color, breed;
-	int legs,eyes;
+
+class Cat {
+	boolean hasFur;
+	String color,breed;
+	int legs, eyes;
 	
 	public void walk() {
-		System.out.println("Cat is Walking");
+		System.out.println("The Cat is walking");
 	}
 	
 	public void eat() {
-		System.out.println("Cat is Eating");
+		System.out.println("The cat is eating");
 	}
+	
+	public void description() {
+		System.out.println
+		("My cat has "+ legs +" legs and "+eyes+" eyes");
+	}
+	
+}
+
+class Dog {
+	
+	String breed, name;
+	public void jump() {
+		System.out.println("My dog name "+name+" jumped");
+	}
+	
+	public void description() {
+		System.out.println
+		("my dog is "+name+" and breed is "+breed);
+	}
+	
+	
 }
 
 public class MainClass {
-	public static void main(String[]args) {
 
+	public static void main(String[] args) {
+		
+//		Cat cat1 = new Cat();
+//		Cat cat2 = new Cat();
+//		
+//		cat1.legs = 3;
+//		cat1.eyes = 2;
+//		
+//		cat2.legs = 4;
+//		cat2.eyes = 1;
+//		
+//		cat1.description();
+//		cat2.description();
+		
+		Dog husky = new Dog();
+		Dog poodle = new Dog();
+		
+		husky.breed = "husky";
+	    husky.name = "Browny";
+	    
+	    poodle.breed = "poodle";
+	    poodle.name = "moolchand";
+
+		poodle.jump();
+		husky.description();
+		
+		
+		
 	}
+
 }
